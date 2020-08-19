8671879fedb1af43cb82d7502cd83f8fd7d023d7
ukjs1cu4 <69923172+ukjs1cu4@users.noreply.github.com>
2020-08-20 00:43:51 +0430

    Update reza.py

--- reza.py
+++ reza.py
@@ -5,12 +5,15 @@ import subprocess
 import time
 
 def reza():
- output = subprocess.check_output("netstat -ntu|awk '{print $5}'|cut -d: -f1 -s|sort|uniq -c|sort -nk1 -r | awk '$1>10'", shell=True)
- ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', output )
- for i in ip:
-  os.system('route add {} reject'.format(i))
-  bantext = "ip : {} Banned".format(i)
-  requests.get("https://api.telegram.org/bot1362187823:AAGgF6XgIJOW9pm9kqboV0l5q6QfHeJ8sw8/sendMessage?chat_id=650249840&text={}".format(bantext))
+ output = subprocess.check_output("netstat -ntu|awk '{print $5}'|cut -d: -f1 -s|sort|uniq -c|sort -nk1 -r | awk '$1>50'", shell=True)
+ if re.findall( r'[0-9]+(?:\.[0-9]+){3}', output ):
+  ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', output )
+  for i in ip:
+   os.system('route add {} reject'.format(i))
+   bantext = "ip : {} Banned".format(i)
+   requests.get("https://api.telegram.org/bot1362187823:AAGgF6XgIJOW9pm9kqboV0l5q6QfHeJ8sw8/sendMessage?chat_id=650249840&text={}".format(bantext))
+ else:
+  reza + "khar"
 schedule.every(1).minutes.do(reza)
 while True:
    schedule.run_pending()
